b496889c6072e4bdd5ce274af9db429feb6c86bb
George Usynin <angiejo03@gmail.com>
2026-01-09 17:17:22 +0700

    removed zksync stuff

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.30;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
 import {DeployMinimal} from "script/DeployMinimal.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
